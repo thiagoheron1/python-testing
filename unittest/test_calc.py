90775758d31fab1eebbe1ee4c3eedaeebfed5109
thiagoheron <thiagoheronavila@gmail.com>
2021-01-15 08:47:23 -0300

    Adding unittest tutorial
    - calc.py -> methods
    - test_calc.py -> unittest of calc.py

---
+++ unittest/test_calc.py
@@ -0,0 +1,48 @@
+"""
+Nome do arquivo: Start with 'test_'
+Nome da Classe: TestNome
+Nome dos métodos precisam iniciar com 'test_'
+
+Assert Methos: https://docs.python.org/3/library/unittest.html#unittest.TestCase.debug
+    - assertEqual
+
+Execute: 
+    - python3 -m unittest test_calc.py (Caso não tenha o IF Main)
+    - python3 test_calc.py (Se tiver o IF Main)
+"""
+
+
+import unittest
+import calc
+
+
+class TestCalc(unittest.TestCase):
+
+    # Crie todos cenários possíveis (edge cases)
+    def test_add(self):
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-1, -1), -2)
+    
+    def test_substract(self):
+        self.assertEqual(calc.subtract(10, 5), 5)
+        self.assertEqual(calc.subtract(-1, 1), -2)
+        self.assertEqual(calc.subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(10, 5), 50)
+        self.assertEqual(calc.multiply(-1, 1), -1)
+        self.assertEqual(calc.multiply(-1, -1), 1)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(10, 5), 2)
+        self.assertEqual(calc.divide(-1, 1), -1)
+        self.assertEqual(calc.divide(-1, -1), 1)
+
+        self.assertRaises(ValueError, calc.divide, 10, 0)
+        # OR
+        with self.assertRaises(ValueError):
+            calc.divide(10, 0)
+
+if __name__ == "__main__":
+    unittest.main()
